2d9cb67db771675438dbf99ec88c2f6691442809
lee <475815059@qq.com>
2018-07-30 19:30:45 +0800

    '安装moogoose,设置参数'

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 nodemon //用于安装后，不用重启服务 
+
+yarn add moongoose
---
+++ src/routes/auth.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import User from '../models/User';
+const router = express.Router();
+
+router.post('/', (req, res) => {
+	const { credentials } = req.body;
+	User.findOne({ email: credentials.email }).then((user) => {
+		if (user) {
+		} else {
+			res.status(400).json({ errors: { global: 'Invalidate credentials!' } });
+		}
+	});
+});
+
+export default router;
